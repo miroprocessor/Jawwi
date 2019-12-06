b74efdc2f41576d7faaacae7662952ffc2994eab
Talha Sayed <talha_sayed@hotmail.com>
2019-12-06 19:12:12 +0300

    Add weather api

---
+++ src/Jawwi/Jawwi.web/Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Jawwi.web.Controllers
+{
+    public class TestController : Controller
+    {
+        public async Task<string> Index()
+        {
+
+            var weatherApi = new WeatherApi.WeatherApi();
+            return await weatherApi.GetCountries("MEA");
+        }
+    }
+}
