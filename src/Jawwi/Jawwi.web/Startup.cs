40948bda1ae841f53b86758d03bf9dcece84bb82
Tariq Abubaker <taq.abubaker@gmail.com>
2019-12-06 21:58:57 +0300

    finished-ui

--- src/Jawwi/Jawwi.web/Startup.cs
+++ src/Jawwi/Jawwi.web/Startup.cs
@@ -10,7 +10,6 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.EntityFrameworkCore;
 
 namespace Jawwi.web
@@ -26,10 +25,10 @@ namespace Jawwi.web
 
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services, IConfiguration config)
+        public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<DataContext>(options => 
-                options.UseSqlServer(config.GetConnectionString("JawwiConnection")));
+            //services.AddDbContext<DataContext>(options => 
+            //    options.UseSqlServer(config.GetConnectionString("JawwiConnection")));
             services.AddControllersWithViews();
         }
 
