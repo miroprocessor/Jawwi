145884254cf7c56a43a63439c1d83d4c34c8ac76
Tariq Abubaker <taq.abubaker@gmail.com>
2019-12-06 22:59:04 +0300

    add-icons + location-page-ui

---
+++ src/Jawwi/Jawwi.web/Controllers/LocationsController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Jawwi.web.Controllers
+{
+    public class LocationsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
